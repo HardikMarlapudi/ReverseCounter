d26022ec759638c319573872dc3771dd0c7d6962
Hardik Marlapudi <hardikmarlapudi@Hardiks-MacBook-Pro.local>
2025-06-24 02:08:01 -0400

    fixed the reverse method

--- script.js
+++ script.js
@@ -2,20 +2,16 @@ let inputNum = document.getElementById("inputNum").value;
 let outputOrder = document.getElementById("outputOrder");
 
 function submit() {
-    let inputNum = document.getElementById("inputNum").value;
+    let inputNum = document.getElementById("inputNum").value.trim();
     let outputOrder = document.getElementById("outputOrder");
-    let numArray = inputNum.split(" ");
-    let numArraySorted = numArray.sort(function(a, b){return b-a});
-    let reverse = "";
 
-    if (inputNum === "") {
+    if (inputNum == "") {
         alert("Please enter a number");
         return;
     }
 
-    for (let i = 0; i < numArraySorted.length; i++) {
-        reverse += numArraySorted[i] + "";
-    }
+    let numArray = inputNum.split(" ");
+    let reversedDigits = numArray.map(num => num.split("").reverse().join(""));
 
-    outputOrder.innerHTML = reverse;
+    outputOrder.innerHTML = reversedDigits.join(" ");
 }
